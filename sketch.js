27b62a3742cdae00b94192559199a762c32ac9d5
hansikaxwilliams <76735967+hansikaxwilliams@users.noreply.github.com>
2021-04-07 21:50:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup(){
+createCanvas(900,400);
+}
+
+function draw(){
+background("lightpink");
+text("This recipe is super easy and can be made within split seconds! Wanna know what am I talking about Lets dive right into it!", 30,80);
+text("the ingredients of this recipe are very less and can be found in your house itself!", 40,100)
+text("The ingredients are as follows-",50,120)
+text("1. TEA",60,140);
+text("2. LEMON JUICE", 70,160);
+text("3. SUGAR", 80,180);
+text("Okay, so here we go...First,you have to boil water and add tea to it(don't boil it too much, let it leave the tint of the color only", 120,200)
+ text("Then, you strain the tea. After this you put the liquid in a glass and add lemon juice and sugar to it.", 140,220)
+ text("If you want you can add mint leaves too! And that's all! Wasn't that super quick!!! You try this recipe too!",160,240);
+drawSprites();
+}
